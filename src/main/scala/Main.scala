37762c89407ae74d87dc5ae7121153be3751a06d
emiliano <emilianotebes@gmail.com>
2021-07-17 00:35:58 -0300

    Several instances

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,28 @@
 package org.et.spark.example
 
+import org.apache.log4j.LogManager
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
+  val LOGGER = LogManager.getRootLogger
+
   def main(args: Array[String]): Unit = {
-    println("Iniciando...")
-    val conf = new SparkConf().setAppName("probando").setMaster("local")
-    val context = new SparkContext(conf)
-    val searches = Seq("hola", "chau", "otra busqueda")
-    val parallel = context.parallelize(searches, 10)
+    LOGGER.info("Iniciando...")
+
+    val conf = new SparkConf().setAppName("probando").setMaster("local[2]")
+    val sparkContext = new SparkContext(conf)
+    val searches = Seq("hola", "chau", "otra busqueda", "otra mas", "Messi", "futbol")
+    val parallel = sparkContext.parallelize(searches)
 
     parallel.foreach(f => {
+      LOGGER.info(s"INICIO $f")
+
       val response = GoogleClient.search(f)
       response match {
-        case Left(value) => println("ERROR" + value)
-        case Right(value) => println(value)
+        case Left(value) => LOGGER.info("ERROR" + value)
+        case Right(value) => LOGGER.info(s"SEARCH RESULT FOR $f")
       }
+      LOGGER.info(s"FIN $f")
     })
   }
 }
