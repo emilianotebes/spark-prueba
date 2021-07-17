7fb58be0c54ed351be83a14a84f55f6f88053486
emiliano <emilianotebes@gmail.com>
2021-07-17 18:39:42 -0300

    Parallel text search

--- src/main/scala/GoogleClient.scala
+++ src/main/scala/GoogleClient.scala
@@ -2,15 +2,32 @@ package org.et.spark.example
 
 import scalaj.http.{Http, HttpResponse}
 
+import scala.util.{Failure, Success, Try}
+
 object GoogleClient {
 
   def search(search: String): Either[Error, String] = {
     val url = s"https://www.google.com/search"
-    val response: HttpResponse[String] = Http(url).param("q", search).asString
-    response.code match {
-      case 200 => Right(response.body)
-      case _ => Left(SomeError(response.body))
+
+    val connectionTry = Try({
+      val response: HttpResponse[String] = Http(url).param("q", search).asString
+      response.code match {
+        case 200 =>
+          Right(response.body)
+        case _ =>
+          Left(SomeError(response.body))
+      }
+    })
+
+    connectionTry match {
+      case Failure(exception) =>
+        Left(SomeError(exception.getMessage))
+      case Success(value) =>
+        value
+      case _ =>
+        Left(SomeError("WTF??"))
     }
+
   }
 }
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 package org.et.spark.example
 
 import org.apache.log4j.LogManager
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
@@ -11,17 +12,19 @@ object Main {
 
     val conf = new SparkConf().setAppName("probando").setMaster("local[8]")
     val sparkContext = new SparkContext(conf)
-    val searches = Seq("hola", "chau", "otra busqueda", "otra mas", "Messi", "futbol")
-    val parallel = sparkContext.parallelize(searches)
 
     val startTime = System.nanoTime
+    val parallel: RDD[String] = sparkContext
+      .textFile("/home/emiliano/Documents/loremIpsum.txt", 10)
+      .flatMap(f => f.split("\\s+"))
+      .filter(f => f != null && f.nonEmpty)
 
     parallel.foreach(f => {
       LOGGER.info(s"INICIO $f")
 
       val response = GoogleClient.search(f)
       response match {
-        case Left(value) => LOGGER.info("ERROR" + value)
+        case Left(err) => LOGGER.error("ERROR" + err.getMessage)
         case Right(value) => LOGGER.info(s"SEARCH RESULT FOR $f")
       }
       LOGGER.info(s"FIN $f")
