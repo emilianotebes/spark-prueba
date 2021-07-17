3e2a272912501f695235afa7b187d67a15dbfcb5
emiliano <emilianotebes@gmail.com>
2021-07-17 01:01:31 -0300

    Several instances. Duration.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,10 +9,12 @@ object Main {
   def main(args: Array[String]): Unit = {
     LOGGER.info("Iniciando...")
 
-    val conf = new SparkConf().setAppName("probando").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("probando").setMaster("local[8]")
     val sparkContext = new SparkContext(conf)
     val searches = Seq("hola", "chau", "otra busqueda", "otra mas", "Messi", "futbol")
     val parallel = sparkContext.parallelize(searches)
+
+    val startTime = System.nanoTime
 
     parallel.foreach(f => {
       LOGGER.info(s"INICIO $f")
@@ -24,5 +26,10 @@ object Main {
       }
       LOGGER.info(s"FIN $f")
     })
+
+    val endTime = System.nanoTime
+    val duration = endTime - startTime
+
+    LOGGER.info("TOTAL DURATION: " + duration)
   }
 }
